c7f16fa464fe5846dddaf2e74287d9fb0d0f1207
NaniNarayanan <narayanan1823@gmail.com>
2022-10-26 23:00:26 +0530

    casino

---
+++ script.js
@@ -0,0 +1,26 @@
+function a(){
+    var a=Math.floor(
+        Math.random()*1000000
+    );
+    var b=Math.floor(
+        Math.random()*1000000
+    );
+    var c=Math.floor(
+        Math.random()*1000000
+    );
+    var d=Math.floor(
+        Math.random()*1000000
+    );
+    var e=Math.floor(
+        Math.random()*1000000
+    );
+    var f=Math.floor(
+        Math.random()*1000000
+    );
+    document.getElementById('id1').innerHTML=a;
+    document.getElementById('id2').innerHTML=b;
+    document.getElementById('id3').innerHTML=c;
+    document.getElementById('id4').innerHTML=d;
+    document.getElementById('id5').innerHTML=e;
+    document.getElementById('id6').innerHTML=f;
+}
